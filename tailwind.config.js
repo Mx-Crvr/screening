7d870531f7d3d20406db8daf3ccf687250546b84
Mx-Crvr <mcevans.dev@gmail.com>
2023-09-25 19:39:48 +0200

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
       xl: '1280px',
     },
     extend: {
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', 'sans-serif']
+      },
       colors: {
         lightblue: "#1e90ff",
       }
